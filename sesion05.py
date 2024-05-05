926871fa683ffef13198219ef246a25f0546a245
Jorge Fernandez <jorferluis@gmail.com>
2024-05-04 22:19:49 -0400

    Clase sesión 05

---
+++ sesion05.py
@@ -0,0 +1,12 @@
+# Valor 10 Entero
+print(10)
+print(type(10))
+
+variable = 100
+print(variable)
+print(type(variable))
+
+# Variable 20 Entero
+variable_2 = int (20)
+print(variable_2)
+print(type(variable_2))
